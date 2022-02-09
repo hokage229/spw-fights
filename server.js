a3f46e2147942cbbbf39dbf0b4ce639f2c27c69c
hokage229 <ernestzulpukarov@gmail.com>
2022-02-10 03:01:08 +0700

    fix

---
+++ server.js
@@ -0,0 +1,26 @@
+// import express, {Request, Response} from 'express'
+//
+// const server = express()
+//
+// server.all("/", (req: Request, res: Response) => {
+//     res.send("bot is running")
+// })
+//
+// const port = process.env.PORT || 5000
+// const keepAlive = () => {
+//     server.listen(port, () => {
+//         console.log("server is ready")
+//     })
+// }
+// export default {keepAlive}
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 5000
+const keepAlive = () => {
+    app.listen(port, () => {
+        console.log("server is ready")
+    })
+}
+export default {keepAlive}
