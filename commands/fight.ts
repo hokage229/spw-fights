484cd4452f04d8292cbb0fed1cf23cb5dab976cf
hokage229 <ernestzulpukarov@gmail.com>
2022-02-14 21:09:03 +0700

    refactor

--- commands/fight.ts
+++ commands/fight.ts
@@ -4,7 +4,7 @@ import {Channel, Interaction, MessageActionRow, MessageButton, Role} from "disco
 export default {
     category: 'Fight',
     description: 'Try your luck to win SPW immunity',
-    slash: false,
+    slash: "both",
     guildOnly: true,
 
     callback: async ({interaction: msgInt, channel, guild}) => {
